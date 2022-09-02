839619461e1bd7d8b30a64d69e5286adefccf5c8
PaulinaAldape <112351776+PaulinaAldape@users.noreply.github.com>
2022-09-02 09:56:24 -0500

    Primer modificación

---
+++ Actividad 3.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Actividad 3
+
+# ### 1.Crea un vector con valores dentro del rango 3 a el numero que representa tu edad.
+
+# In[1]:
+
+
+import numpy as np
+
+
+# In[5]:
+
+
+matriz1=np.array(list(range(3,26)), dtype="int")
+
+
+# In[6]:
+
+
+matriz1
+
+
+# ### 2.Crea un arreglo con los siguientes elementos:
+
+# In[7]:
+
+
+matriz2=np.array([0,1,2,3,4,0,1,2,3,4],dtype="int")
+
+
+# In[8]:
+
+
+matriz2
+
+
+# ### 3.Ordena de forma ascendiente dicho arreglo.
+
+# In[11]:
+
+
+matriz2.sort()
+
+
+# In[14]:
+
+
+matriz2
+
